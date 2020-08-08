6f1a078498c3953738dcd68d8396dd34006dc195
Kurt V <kjvoigtritter@gmail.com>
2020-08-08 15:26:32 -0700

    revised prioqueue

--- customQueue.py
+++ customQueue.py
@@ -1,13 +1,16 @@
-#import card
+import card
 
-#priority queue fit to this application
+#priority queue fit to the needs of this application
 
 class Node: #card can be said to be a value for now, but later implement for a card object
   def __init__(self, card):
-    self.card = card
+    self.cardObject = card
     self.left = None
     self.right = None
     self.height = 1
+  def getCardWeight(self):
+    return self.cardObject.weight
+    
 
 
 class AVL_Tree():
@@ -47,15 +50,15 @@ class AVL_Tree():
     leftNode.height = 1 + max(self.getHeight(leftNode.left), self.getHeight(leftNode.right))
     return leftNode
   
-    #insert a new element into tree
-  def insert(self, root, key):
+    #insert a new card into tree
+  def insert(self, root, newCard):
     #insert node into place based on value (like a normal binary search tree)
     if root is None:
-      return Node(key)
-    elif key < root.card:
-      root.left = self.insert(root.left, key)
+      return Node(newCard)
+    elif newCard.weight < root.cardObject.weight:
+      root.left = self.insert(root.left, newCard)
     else:
-      root.right = self.insert(root.right, key)
+      root.right = self.insert(root.right, newCard)
       
     #update heights of all nodes after this insertion is done
     root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
@@ -64,44 +67,64 @@ class AVL_Tree():
     balance = self.getBalance(root)
     
     #left left case
-    if balance > 1 and key < root.left.card: 
+    if balance > 1 and newCard.weight < root.left.cardObject.weight: 
       return self.rightRotate(root) 
   
     # right right case 
-    if balance < -1 and key > root.right.card: 
+    if balance < -1 and newCard.weight > root.right.cardObject.weight: 
       return self.leftRotate(root) 
   
     # left right 
-    if balance > 1 and key > root.left.card: 
+    if balance > 1 and newCard.weight > root.left.cardObject.weight: 
       root.left = self.leftRotate(root.left) 
       return self.rightRotate(root) 
   
     #right left 
-    if balance < -1 and key < root.right.card: 
+    if balance < -1 and newCard.weight < root.right.cardObject.weight: 
       root.right = self.rightRotate(root.right) 
       return self.leftRotate(root) 
   
     return root 
       
       
-    
-     
+  #finds minimum element in tree (in order to display it)  
+  def findMin(self, root):
+    if root is None:
+      return
+    if root.left is None:
+      return root.cardObject.weight
+    return self.findMin(root.left)
    
        
   def preOrder(self, root):
-         if root is None:
-           return
-         print("{0}".format(root.card), end="")
-         self.preOrder(root.left)
-         self.preOrder(root.right)
-         
+    if root is None:
+      return
+    
+    
+    self.preOrder(root.left)
+    
+    self.preOrder(root.right)
+    print(root.getCardWeight())
+
+
+
 myTree = AVL_Tree() 
 root = None
-  
-root = myTree.insert(root, 10) 
-root = myTree.insert(root, 20) 
-root = myTree.insert(root, 30) 
-root = myTree.insert(root, 40) 
-root = myTree.insert(root, 50) 
-root = myTree.insert(root, 25) 
-myTree.preOrder(root)
+#testNode = card.Card("name","word", "translation", "sentence", 0.5)
+#print(testNode.cardObject.weight)
+#print(testNode.card.name)
+#testNode = Node(testCard)
+root = myTree.insert(root, card.Card("name","word", "translation", "sentence", 30))
+root = myTree.insert(root, card.Card("name","word", "translation", "sentence", 10))
+root = myTree.insert(root,  card.Card("name","word", "translation", "sentence", 60))
+root = myTree.insert(root, card.Card("name","word", "translation", "sentence", 50))
+root = myTree.insert(root, card.Card("name","word", "translation", "sentence", 65))
+root = myTree.insert(root,  card.Card("name","word", "translation", "sentence", 55))
+print(myTree.findMin(root))
+#root = myTree.insert(root, 10) 
+#root = myTree.insert(root, 20) 
+#root = myTree.insert(root, 30) 
+#root = myTree.insert(root, 40) 
+#root = myTree.insert(root, 50) 
+#root = myTree.insert(root, 25) 
+#myTree.preOrder(root)
